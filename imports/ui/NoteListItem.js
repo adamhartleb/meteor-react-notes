2d25d942a50f640eb458899fc730f3d5afd38782
Adam <adamjhartleb@gmail.com>
2017-04-03 12:27:29 -0700

    Added SCSS mixins

--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -7,8 +7,8 @@ export const NoteListItem = ({ updatedAt, title, _id, Session, selected }) => {
   const className = selected ? 'item item--selected' : 'item'
   return (
     <div className={className} onClick={() => Session.set('selectedNoteId', _id)}>
-      <h5>{title || 'Untitled'}</h5>
-      <p className='item--date'>{moment(updatedAt).format('M/DD/YY')}</p>
+      <h5 className='item__title'>{title || 'Untitled'}</h5>
+      <p className='item__subtitle'>{moment(updatedAt).format('M/DD/YY')}</p>
     </div>
   )
 }
